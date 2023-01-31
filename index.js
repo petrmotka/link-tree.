9d550118c7ec844dba655065e7d7b8ef8e1ef192
petrmotka <101020938+petrmotka@users.noreply.github.com>
2023-01-31 15:06:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("hello world");
+
+
+
+
+function  instagram () {
+
+open("https://www.instagram.com/petrmotka");
+
+
+
+};
+
+function twitter () {
+
+
+open ("https://twitter.com/petrmotka69");
+
+};
+
+function github () {
+
+open("https://github.com/petrmotka");
+
+
+
+};
+
+
